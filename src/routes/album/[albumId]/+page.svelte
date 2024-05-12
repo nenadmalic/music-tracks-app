3b7d41f45c652179c3d653b119d3c1c9a4266288
Nenad Malic <4789424+nenadmalic@users.noreply.github.com>
2024-05-12 20:48:59 +0200

    Duration without decimals.

--- src/routes/album/[albumId]/+page.svelte
+++ src/routes/album/[albumId]/+page.svelte
@@ -4,3 +4,23 @@
 </script>
 
 <h1 class="is-size-1">{data.album.albumTitle}</h1>
+<p>{data.album.artistName}</p>
+
+<table class="table mt-6">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Track</th>
+            <th>Duration</th>
+        </tr>
+    </thead>
+    <tbody>
+        {#each data.tracks as track, i}
+            <tr>
+                <td>{i + 1}</td>
+                <td>{track.trackName}</td>
+                <td>{Math.floor(track.trackMs / 1000)} s</td>
+            </tr>
+        {/each}
+    </tbody>
+</table>
